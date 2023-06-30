b22c539073a00982400b5f4f386d519cadf09e38
mccrush <mccrush2027@gmail.com>
2023-06-30 13:20:38 +0700

    Settings listitems view

--- src/components/interface/TheSideBar.vue
+++ src/components/interface/TheSideBar.vue
@@ -1,38 +1,48 @@
 <template>
-    <div class="side-bar bg-dark vh-100">
-        <div class="text-center bg-light p-3">SKCRM</div>
+  <div class="side-bar bg-dark vh-100">
+    <div class="text-center bg-white fw-bold p-3">SKCRM</div>
 
-        <div class="list-items p-2">
-            <div v-for="item in menuItems" :key="item.id" class="btn-group btn-group-sm w-100 mt-1">
-                <router-link :to="'/list/' + item.type" class="btn btn-secondary w-75">
-                    {{ item.title }}</router-link>
+    <div class="list-items p-2">
+      <div
+        v-for="item in menuItems"
+        :key="item.id"
+        class="btn-group btn-group-sm w-100 mt-1"
+      >
+        <router-link :to="'/list/' + item.type" class="btn btn-secondary w-75">
+          {{ item.title }}</router-link
+        >
 
-                <button type="button" class="btn btn-secondary w-25" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                    @click="$emit('show-modal', { type: item.type, mod: 'create' })">
-                    +</button>
-            </div>
-        </div>
+        <button
+          type="button"
+          class="btn btn-secondary w-25"
+          data-bs-toggle="modal"
+          data-bs-target="#staticBackdrop"
+          @click="$emit('show-modal', { type: item.type, mod: 'create' })"
+        >
+          +
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import { dataModels } from './../../data/dataModels'
 
 export default {
-    emits: ['show-modal'],
-    computed: {
-        menuItems() {
-            return dataModels.filter(item => item.menu)
-        }
+  emits: ['show-modal'],
+  computed: {
+    menuItems() {
+      return dataModels.filter(item => item.menu)
     }
+  }
 }
 </script>
 
 
 <style scoped>
 .side-bar {
-    width: 200px;
-
+  width: 200px;
 }
 
 /* 
--- src/pages/PageListItems.vue
+++ src/pages/PageListItems.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="row">
-    <div class="col-12 bg-white shadow-sm p-3">List</div>
+    <div class="col-12 bg-white p-3">List</div>
 
     <div class="col-12 p-3">
-      <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
-          The current link item
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+      <div v-if="listItems.length" class="list-group">
+        <a
+          v-for="item in listItems"
+          :key="item.id"
+          href="#"
+          class="list-group-item list-group-item-action bg-white border-0"
+          >{{
+            item.city +
+            ' - ' +
+            item.name +
+            ' - ' +
+            item.typek +
+            ' - ' +
+            item.power +
+            ' - ' +
+            getLocaleDateFromDateDigit(item.dateCreate)
+          }}</a
+        >
       </div>
     </div>
 
@@ -19,7 +32,16 @@
 </template>
 
 <script>
-export default {
+import { getLocaleDateFromDateDigit } from './../helpers/getDateFormat'
 
+export default {
+  computed: {
+    listItems() {
+      return this.$store.getters[this.$route.params.type]
+    }
+  },
+  methods: {
+    getLocaleDateFromDateDigit
+  }
 }
 </script>
