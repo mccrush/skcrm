271ed221be938f4fc40016119f24ece95d1172b1
mccrush <mccrush2027@gmail.com>
2023-08-01 11:29:28 +0700

    Update name of page

--- src/data/dataModels.js
+++ src/data/dataModels.js
@@ -4,7 +4,7 @@ export const dataModels = [
   { id: '03', type: 'order', title: 'Заказы', menu: true },
   { id: '04', type: 'kotel', title: 'Котлы', menu: true },
   { id: '05', type: 'ograda', title: 'Оградки', menu: false },
-  { id: '06', type: 'stage', title: 'Этапы', menu: true },
+  { id: '06', type: 'stage', title: 'Этапы З', menu: true },
   { id: '07', type: 'stageObrabotka', title: 'Этапы О', menu: true },
   { id: '08', type: 'obrabotka', title: 'Обработки', menu: true },
   { id: '09', type: 'user', title: 'Сотрудники', menu: true },
