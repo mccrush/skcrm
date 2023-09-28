17bc17f623e3b0eee798a5c683acbc0133326c26
mccrush <mccrush2027@gmail.com>
2023-09-28 12:09:23 +0700

    Update Navbar view

--- src/components/interface/TheNavbar.vue
+++ src/components/interface/TheNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navbar row bg-white shadow-sm z-2 p-3">
-    <div v-if="user" class="col-12 d-md-none d-flex justify-content-between">
+    <div v-if="user" class="col-6 d-md-none">
       <button
         class="btn btn-sm btn-light ps-3 pe-3"
         type="button"
@@ -14,7 +14,7 @@
       <button
         v-if="$route.params.type && $route.params.type !== 'production'"
         type="button"
-        class="btn btn-sm btn-dark d-md-none ps-3 pe-3"
+        class="btn btn-sm btn-dark d-md-none w-50 ms-2 ps-3 pe-3"
         data-bs-toggle="modal"
         data-bs-target="#modalWindow"
         @click="
@@ -28,11 +28,12 @@
         +
       </button>
     </div>
+    <div class="col-6 d-md-none text-end"><slot name="count"></slot></div>
     <div class="col-12">
       <div class="row mt-2 mt-md-0">
-        <div class="col-4 col-md-2"><slot name="count"></slot></div>
+        <div class="col-md-2 d-none d-md-block"><slot name="count"></slot></div>
         <div class="col-8 col-md-3"><slot name="filter"></slot></div>
-        <div class="col-12 col-md-2 mt-2 mt-md-0 ps-0 pe-0">
+        <div class="col-12 col-md-2 mt-md-0 ps-0 pe-0">
           <slot name="filterSelect"></slot>
         </div>
         <div class="col-12 col-md-5 mt-2 mt-md-0">
