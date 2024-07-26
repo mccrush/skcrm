6a21237b90181262420801b765926af8af8fa076
mccrush <mccrush2027@gmail.com>
2024-07-26 14:27:46 +0700

    Update filter for items

--- src/modules/order/OrderKanban.vue
+++ src/modules/order/OrderKanban.vue
@@ -111,7 +111,7 @@ export default {
         if (stagePosition === 2) {
           //console.log('stagePosition 2 = ', stagePosition)
           const stagesProductionId = this.stagesProduction.filter(
-            item => item.position >= 2 && item.position <= 8
+            item => item.position >= 2 && item.position <= 7
           )
 
           //console.log('stagesProductionId = ', stagesProductionId)
